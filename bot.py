b60b64f1a2f8fe22bf74aedc1782710d25e24390
Rümeysa <70983764+rumeysaguc@users.noreply.github.com>
2024-08-29 21:54:30 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -15,6 +15,19 @@ async def hello(ctx):
     await ctx.send(f'Merhaba! Ben {bot.user}, bir Discord sohbet botuyum!')
 
 @bot.command()
+async def siberguvenliknedir(ctx):
+    await ctx.send("Siber güvenlik; bilgisayarları, sunucuları, mobil cihazları, elektronik sistemleri, ağları ve verileri kötü amaçlı saldırılardan koruma uygulamasıdır. Bilgi teknolojisi güvenliği veya elektronik bilgi güvenliği olarak da bilinir. ")
+
+
+@bot.command()
+async def linuxnedir(ctx):
+    await ctx.send("Linux, açık kaynaklı bir işletim sistemidir")
+
+@bot.command()
+async def beyazsapkalıhackernedir(ctx):
+    await ctx.send("eyaz Şapkalı Hacker, bilişim suçları işleyen kötü niyetli kişilerin kullanmış oldukları teknik ve yöntemleri iyi bir şekilde öğrenmiş, siber korsanların eylemleri sırasında kullandıkları araçları ve yazılımları tanıyan, aynı bilgi ve beceriye sahip, iyi niyetli siber güvenlik güvenlik uzmanlarıdır. ")
+  
+@bot.command()
 async def heh(ctx, count_heh = 5):
     await ctx.send("he" * count_heh)
 
